ac8a34b379eb8d39c044ca4e5eb95a7b66a6f4e5
Ryan Rizk <ryanrizk@cmail.carleton.ca>
2024-03-06 23:43:39 -0500

    Ryan: feat: added main_pi4.py

---
+++ RaspberryPi4/main_pi4.py
@@ -0,0 +1,16 @@
+from helperFunctions import IPService, messageService, sauceService
+
+# Main program to run Pi#4 and delegate tasks to helper functions
+def main():
+
+    # Get my own IP address, I'm using Pi#4
+    pi4_ip = IPService.get_local_ip_address(1)
+    # Upload my Pi IP address to Firebase
+    IPService.save_ip(pi4_ip)
+
+    # Retrieve Pi#1 IP addresses
+    pi1_ip = IPService.get_ip("Pi1")
+    # Retrieve Pi#2 IP addresses
+    pi2_ip = IPService.get_ip("Pi2")
+    # Retrieve Pi#3 IP addresses
+    pi3_ip = IPService.get_ip("Pi3")
